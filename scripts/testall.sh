a1763ac351de32d8c675a873ee97cf6a8237bd3d
Oleskii Oleksenko <alexo_o@ukr.net>
2017-02-14 10:40:38 +0100

    useful scripts

--- scripts/testall.sh
+++ scripts/testall.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+cd $COMP_BENCH
+
+echo "Phoenix"
+./fex.py run -n phoenix_perf -t gcc_native gcc_asan --multithreaded_build -i test
+
+echo ""
+echo "Parsec"
+./fex.py run -n parsec_perf -t gcc_native gcc_asan --multithreaded_build -i test
+
+cd -
