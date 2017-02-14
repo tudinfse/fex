90245d472564e81063b54c5fd8a5367f33883c93
Oleskii Oleksenko <alexo_o@ukr.net>
2017-02-14 16:59:41 +0100

    benchmarks: removed all library sources and included them into the installation step

--- install/benchmarks/apache.sh
+++ install/benchmarks/apache.sh
@@ -8,9 +8,40 @@ source ${COMP_BENCH}/install/common.sh
 # ============
 # apache
 # ============
+echo "Preparing Apache..."
 SRC_PATH="${BIN_PATH}/httpd/src"
 VERSION="2.4.18"
 
 mkdir -p ${BIN_PATH}/httpd
 download_and_link httpd-${VERSION} https://archive.apache.org/dist/httpd/httpd-${VERSION}.tar.gz ${SRC_PATH}
 
+# ============
+# Libs
+# ============
+echo "Downloading libraries..."
+cd ${DATA_PATH}/
+
+if [ -d "apache_libs" ]; then
+    rm -rf apache_libs/
+fi
+mkdir apache_libs/
+
+set +e
+wget -nc https://wwwpub.zih.tu-dresden.de/~s7030030/apache_libs.tar.gz
+set -e
+
+tar xf apache_libs.tar.gz -C apache_libs/
+rm apache_libs.tar.gz
+
+cd -
+
+echo "Preparing libraries..."
+cd ${DATA_PATH}/apache_libs/
+
+for lib in *; do
+    if [ -d "${lib}" ]; then
+        cp -r ${lib}/src/ ${COMP_BENCH}/src/libs/${lib}/src/
+    fi
+done
+
+echo "Apache installed"
--- install/benchmarks/parsec.sh
+++ install/benchmarks/parsec.sh
@@ -8,11 +8,11 @@ apt-get install -y pkg-config gettext \
                    libbsd-dev libx11-dev x11proto-xext-dev libxext-dev libxt-dev libxi-dev libxmu-dev \
                    libglib2.0-dev
 
-echo "=== Downloading inputs ==="
+echo "Downloading inputs..."
 cd ${DATA_PATH}/
 
 if [ -d "inputs" ]; then
-    rm -rf inputs/
+    rm -rf inputs/parsec/
 fi
 mkdir inputs/
 
@@ -24,5 +24,31 @@ tar xf inputs.tar.gz -C inputs/
 rm inputs.tar.gz
 
 cd -
+
+echo "Downloading libraries..."
+cd ${DATA_PATH}/
+
+if [ -d "parsec_libs" ]; then
+    rm -rf parsec_libs/
+fi
+mkdir parsec_libs/
+
+set +e
+wget -nc https://wwwpub.zih.tu-dresden.de/~s7030030/parsec_libs.tar.gz
+set -e
+
+tar xf parsec_libs.tar.gz -C parsec_libs/
+rm parsec_libs.tar.gz
+
+cd -
+
+echo "Preparing libraries..."
+cd ${DATA_PATH}/parsec_libs/
+
+for lib in *; do
+    if [ -d "${lib}" ]; then
+        cp -r ${lib}/src/ ${COMP_BENCH}/src/libs/${lib}/src/
+    fi
+done
 
 echo "Parsec installed"
