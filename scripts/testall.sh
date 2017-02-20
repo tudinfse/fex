6444e8682091969cda365f72cfa88d7dc87e1c2e
Oleskii Oleksenko <alexo_o@ukr.net>
2017-02-20 13:31:38 +0100

    see b7856b89

--- scripts/testall.sh
+++ scripts/testall.sh
@@ -2,17 +2,27 @@
 
 cd $PROJ_ROOT
 
-printf "\n  ==  SUITES  ==\n"
+printf "\n  ==  MICRO  ==\n"
+
+printf "\n  ==  BENCHMARK SUITES  ==\n"
 
 echo "  ==  Phoenix  =="
 ./fex.py run -n phoenix_perf -t gcc_native gcc_asan --multithreaded_build -i test
 
+printf "\n  ==  Splash  ==\n"
+
 printf "\n  ==  Parsec  ==\n"
 ./fex.py run -n parsec_perf -t gcc_native gcc_asan --multithreaded_build -i test
+
 
 printf "\n  ==  APPS  ==\n"
 
 printf "\n  ==  Apache  ==\n"
-./fex.py run -n apache_perf -t gcc_native gcc_asan --multithreaded_build
+./fex.py run -n apache_perf -t gcc_native gcc_asan --multithreaded_build -i test
+
+printf "\n  ==  Memcached  ==\n"
+printf "\n  ==  Nginx  ==\n"
+printf "\n  ==  Postgres  ==\n"
+printf "\n  ==  SQLite  ==\n"
 
 cd -
